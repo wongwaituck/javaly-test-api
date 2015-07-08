5205b4d6b484cff6d3fefde47a2f7b930855fd8c
Wai Tuck <wongwaituck@gmail.com>
2015-07-08 21:35:13 +0800

    Created method injector

---
+++ test-api/nodescripts/methodinjector.js
@@ -0,0 +1,11 @@
+var text = 'public static int add(int a, int b){return (a + b);}';
+
+function generateUserJavaCode (userMethod) {
+  var fileName = 'MethodHolder.java';
+  var fileContents = 'public class MethodHolder{ e35089b2d968d2c00562279dd210847f3e156caa7c9affbaa45a25c6c0e75edf }';
+  var insertionHash = 'e35089b2d968d2c00562279dd210847f3e156caa7c9affbaa45a25c6c0e75edf'; //SHA-256 of ILoveJava
+  fileContents = fileContents.replace(insertionHash, userMethod);
+  return fileContents;
+}
+
+console.log(generateUserJavaCode(text));
